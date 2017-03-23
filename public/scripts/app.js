298e0359249272f10fb703514101d9bcfb6d3ad5
Sumeet Minhas <sumeetm273@gmail.com>
2017-03-23 04:15:38 +0000

    toggle compose box

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,35 +4,12 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
  $(document).ready(function() {
-var tweetData = [{
-  "user": {
-    "name": "Newton",
-    "avatars": {
-      "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-    },
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}];
 
 function renderTweets(tweets) {
+  $('#tweets').empty();
   for (let tweet in tweets){
     $(createTweetElement(tweets[tweet])).prependTo('#tweets');
   }
-}
-
-function escapeIt(str){
-  return str
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
 }
 
 function createTweetElement(tweet) {
@@ -53,28 +30,72 @@ function createTweetElement(tweet) {
 
 function createHeader(data) {
   var $header = $('<header></header>')
-    $header.append($('<img class="avatar">'))
+    $header.append($('<img>').addClass('avatar').attr('src', data.user.avatars.small))
            .append($('<h3></h3>').text(data.user.name))
-           .append($('<span class="user-handle"></span>').text(`@${data.user.handle}`));
+           .append($('<span>').addClass("user-handle").text(`${data.user.handle}`));
     return $header;
 }
 
 function createBody(data) {
-  var $body = $('<main></main>')
-    $body.append($('<p></p>').text(data.content.text));
+  var $body = $('<div>')
+    $body.append($('<p>').text(data.content.text));
   return $body;
 }
 
 function createFooter(data) {
-  var $footer = $('<footer></footer>')
-    $footer.append($('<p></p>').text(data.created_at.number));
+  var $footer = $('<footer>')
+    $footer.append($('<p>').text(data.created_at));
   return $footer;
 }
-//event handlers
-  $('form').on('submit', function (e) {
-    e.preventDefault();
-  })
 
-renderTweets(tweetData);
+function loadTweets() {
+  $.ajax ({
+    method: 'GET',
+    url: '/tweets',
+    success: function(tweets) {
+      renderTweets(tweets);
+    },
+    error: function() {
+      // alert('error');
+    }
+  })
+}
+//event handlers
+
+$('#usr-nav').on('click', function(e){
+    $('section.new-tweet').slideToggle();
+    $('section textarea').focus();
+  });
+
+$('.new-tweet form').on('submit', function (e) {
+  e.preventDefault();
+
+  $('#error').text("");
+  var $newTweet = $('.new-tweet textarea');
+  var $newTweetText = $newTweet.val().trim();
+  alert($newTweetText);
+  if ($newTweetText.length === 0) {
+    $('#error').text("Tweet area cannot be empty.");
+  } else if ($newTweetText.length > 140) {
+    $('#error').text("Tweet exceeds 140 characters.");
+  } else {
+  $.ajax ({
+    method: 'POST',
+    url: '/tweets',
+    data: {
+      text: $newTweetText
+    },
+    success: function() {
+      // alert('success');
+      loadTweets();
+    },
+    error: function() {
+      // alert('error');
+    }
+  })
+}
+});
+
+loadTweets();
 
 })
